866c719592b0be2c614daa56210c251e1323a001
Kavinduherarth_github <151900001+kavindugit@users.noreply.github.com>
2025-05-05 14:14:09 +0530

    pr

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,18 +10,14 @@ import 'react-toastify/dist/ReactToastify.css';
 import Gallery from "./pages/Gallery"
 import SignUp from "./pages/Auth/SignUp"
 
-import AdultRegistrationForm from "./pages/Admin/adultRegistrationForm"
-import DoctorRegistration from "./pages/Admin/DoctorRegistration"
-import EmployeeApplication from "./pages/Auth/EmployeeApplication"
-import UserProfile from "./pages/UserManagement/userProfile"
-import Dashboard from "./pages/InventoryManagement/Dashboard"
-
-
+import AdultRegistrationForm from "./pages/Admin/AdultRegistrationForm"
+import DoctorRegistrationForm from "./components/AdminPanel/RegistationForms/DoctorRegistrationForm"
 import EmployeeApplication from "./pages/Admin/EmployeeApplication"
-
+import UserProfile from "./pages/User/UserProfile"
+import Dashboard from "./pages/InventoryManagement/Dashboard"
 import AdminPanel from "./pages/Admin/AdminPanel"
 import AdultProfile from "./pages/User/AdutProfile"
-import UserProfile from "./pages/User/UserProfile"
+
 import GuardianProfile from "./pages/User/GuardianProfile"
 
 import NurseProfile from "./pages/User/NurseProfile"
@@ -30,7 +26,7 @@ import CaregiverProfile from "./pages/User/CaregiverProfile"
 import AdminSessionTable from "./pages/Reservations/AdminSessionTable"
 import Services from "./components/User/Services"
 
-import AdultRegistrationForm from "./pages/Admin/AdultRegistrationForm"
+
 import AddAppointment from "./pages/Reservations/AddAppointment"
 import Booking from "./pages/Reservations/Booking"
 import CreateSessionForm from "./pages/Reservations/CreateSessionForm"
