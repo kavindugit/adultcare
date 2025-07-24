82d678b49eb42db828f9ce7275aedb72fac790fd
Kavinduherarth_github <151900001+kavindugit@users.noreply.github.com>
2025-07-24 12:19:45 +0530

    few

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ import reservationRouter from './routes/reservationRoutes.js';
 import parcelRouter from './routes/packagesRoute.js';
 
 import prescriptionRouter from './routes/prescriptionRoutes.js';
-
+//hii
 import shedulerouter from './routes/sheduleRoutes.js';
 import packageRequestRouter from './routes/packageRequestRouter.js';
 import healthReportRouter from './routes/healthReports.js';
