1b5d5bef908a05623a5a3505547cbe98c96f1cb4
Kavinduherarth_github <151900001+kavindugit@users.noreply.github.com>
2025-07-24 14:58:07 +0530

    test22

--- server/server.js
+++ server/server.js
@@ -27,6 +27,7 @@ import parcelRouter from './routes/packagesRoute.js';
 import prescriptionRouter from './routes/prescriptionRoutes.js';
 
 //hii how are you  ergrergreggergeregereg
+//how are you doing today?
 
 import shedulerouter from './routes/sheduleRoutes.js';
 import packageRequestRouter from './routes/packageRequestRouter.js';
